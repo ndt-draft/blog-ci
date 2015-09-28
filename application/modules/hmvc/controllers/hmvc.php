c55311a52cade6d8b1e4b2ea306254b9f8e53570
thanh4890 <thanh4890@gmail.com>
2015-09-28 14:03:43 +0700

    remove sparks

--- application/modules/hmvc/controllers/hmvc.php
+++ application/modules/hmvc/controllers/hmvc.php
@@ -1,14 +1,10 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
  
 class Hmvc extends MX_Controller {
- 
    public function index(  )
    {
       $this->load->view('hmvc_view');
-      $this->load->spark('example-spark/1.0.0');      # We always specify the full path from the spark folder
-		$this->example_spark->printHello();             # echo's "Hello from the example spark!"
    }
 }
- 
 /* End of file hmvc.php */
 /* Location: ./application/widgets/hmvc/controllers/hmvc.php */
