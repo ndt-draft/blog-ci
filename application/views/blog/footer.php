5d1751bd74622ef8fa281376407bbcd7489888ec
thanh4890 <thanh4890@gmail.com>
2015-09-29 14:42:17 +0700

    change menu structure

--- application/views/blog/footer.php
+++ application/views/blog/footer.php
@@ -1,5 +1,6 @@
-	</div> <!-- .site -->
-	<script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
-	<script src="<?php echo base_url('assets/js/main.js'); ?>"></script>
+    </div> <!-- .site -->
+    <script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+    <script src="<?php echo base_url('assets/js/bootstrap.min.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/js/main.js'); ?>"></script>
 </body>
 </html>
--- application/views/blog/header.php
+++ application/views/blog/header.php
@@ -10,16 +10,47 @@
 </head>
 <body>
     <div class="site">
-        <nav class="navigation">
-            <ul class="main-navigation nav nav-tabs">
-                <li class="<?php echo isset($is_home) ? 'active' : ''; ?>">
-                    <?php echo anchor('blog', 'Home'); ?>
-                </li>
-                <li class="<?php echo isset($is_create) ? 'active' : ''; ?>">
-                    <?php echo anchor('blog/create', 'Create'); ?>
-                </li>
-                <li class="<?php echo isset($is_search) ? 'active' : ''; ?>">
-                    <?php echo anchor('blog/search', 'Search'); ?>
-                </li>
-            </ul> <!-- .main-navigation -->
-        </nav> <!-- .navigation -->
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="<?php echo base_url('blog'); ?>">Blog CI</a>
+                </div>
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+                        <li class="<?php echo isset($is_home) ? 'active' : ''; ?>">
+                            <?php echo anchor('blog', 'Home'); ?>
+                        </li>
+                        <li class="<?php echo isset($is_create) ? 'active' : ''; ?>">
+                            <?php echo anchor('blog/create', 'Create'); ?>
+                        </li>
+                        <li class="<?php echo isset($is_search) ? 'active' : ''; ?>">
+                            <?php echo anchor('blog/search', 'Search'); ?>
+                        </li>
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#">Sign up</a></li>
+                        <li><a href="#">Sign in</a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">John Doe <span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="">Signed in as <strong>john-doe</strong></a></li>
+                                <li role="separator" class="divider"></li>
+                                <li><a href="#">Your Profile</a></li>
+                                <li role="separator" class="divider"></li>
+                                <li><a href="#">Settings</a></li>
+                                <li><a href="#">Change Password</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div><!-- /.navbar-collapse -->
+            </div><!-- /.container-fluid -->
+        </nav>
